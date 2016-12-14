b766b3f9d2da9476c39428383527d7250c73fc94
ujishu <ujishu2010@gmail.com>
2016-12-14 19:07:20 +0200

    spider upgrade

--- ebayCrawler/spiders/espider.py
+++ ebayCrawler/spiders/espider.py
@@ -1,4 +1,4 @@
-#This spider pull next info from ebay products list:
+#This spider pull next info from ebay product search list:
 # Name, link, cost, country, img. url
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
@@ -7,21 +7,45 @@ from scrapy.selector import Selector
 
 class espider(CrawlSpider):
     name = "espider"
-    download_delay = 2
+    download_delay = 4
     allowed_domains = ['ebay.com']
-    start_urls = ['http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw=coffee+maker&_pgn=1&_skc=100&rt=nc']
+    start_urls = ['http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw=coffee+maker&_ipg=25&rt=nc']
 
     rules = (
-    	Rule(lle(allow=r'http://www\.ebay\.com/sch/i\.html\?_from=R40&_sacat=0&_nkw=coffee\+maker&_pgn=[1-10]&_skc=100&rt=nc'), 
+    	Rule(lle(allow=r'http://www\.ebay\.com/sch/i\.html\?_from=R40&_sacat=0&_nkw=coffee\+maker&_ipg=25&rt=nc'), 
     		callback='parse_item', follow=True),
     	)
 
     def parse_item(self, response):
     	nameList = response.xpath('//*[@id="ListViewInner"]/li/h3/a/text()').extract()
         linkList = response.xpath('//*[@id="ListViewInner"]/li/h3/a/@href').extract()
-        costList = response.xpath('//*[@id="ListViewInner"]/li/ul[@class="lvprices left space-zero"]').extract()
+        
+        raw_costList = response.xpath('//*[@id="ListViewInner"]/li/ul[@class="lvprices left space-zero"]').extract()
+        costList = []
+        for htmlpart in raw_costList:#extract first cost
+            raw_cost = Selector(text=htmlpart).xpath('//ul[@class="lvprices left space-zero"]/li[@class="lvprice prc"]/span/text()').extract_first()
+            try:
+                result = raw_cost[raw_cost.index('$')+1:]
+            except:
+                result = '-'
+            costList.append(result)
+        
+        countryList_raw = response.xpath('//*[@id="ListViewInner"]/li/ul[@class="lvdetails left space-zero full-width"]/li/text()').extract()
+        countryList = []
+        for cnt in countryList_raw:# clear country list
+            if cnt.isspace() == False:
+                cnt = cnt[cnt.index('F'):]
+                countryList.append(cnt)
+            else:
+                pass
 
-        print costList
-        #yield {
-    	#"name" : response.xpath('//*[@id="ListViewInner"]/li/h3/a/text()').extract()
-    	#}
+        imgUrlList = response.xpath('//*[@id="ListViewInner"]/li/div/div/a/img/@src').extract()
+
+        for i in range(len(nameList)):
+            yield {
+            "name" : nameList[i],
+            "link" : linkList[i],
+            "cost" : costList[i],
+            "country" : countryList[i],
+            "imgUrl" : imgUrlList[i]
+            }
